e1a9cf2352edef9ebf0fdde8a6886db58bf4e90f
Lindsey Heagy <lindseyheagy@gmail.com>
2017-05-17 09:57:29 -0700

    add list to the nitpick ignore. I think this is related to sphinx-doc/sphinx#3320, which has been fixed on sphinx-doc/sphinx#3744, but not necessarily released yet

--- docs/conf.py
+++ docs/conf.py
@@ -301,3 +301,7 @@ def _supress_nonlocal_image_warn(self, msg, node, **kwargs):
         self._warnfunc(msg, '{0!s}:{1!s}'.format(*get_source_line(node)))
 
 supress_nonlocal_image_warn()
+
+nitpick_ignore = [
+    ('py:obj', 'list'),
+]
