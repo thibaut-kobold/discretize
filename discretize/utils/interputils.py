12b521bdd81773f47fe37d3109c9d08195881106
Rowan Cockett <rowanc1@gmail.com>
2018-04-02 19:10:58 -0600

    Add private things so treemesh works.

--- discretize/utils/interputils.py
+++ discretize/utils/interputils.py
@@ -1,1 +1,4 @@
 from matrixutils import interpmat
+from matrixutils.interputils import _interpmat1D
+from matrixutils.interputils import _interpmat2D
+from matrixutils.interputils import _interpmat3D
